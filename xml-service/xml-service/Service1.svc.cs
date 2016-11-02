45a9673151edbb60d9b736fb34ddd8c4017fd5e8
Michael Zarate (Student) <mczarate@sundevils.asu.edu>
2016-11-01 18:38:22 -0700

    Added two functions to my service

--- xml-service/xml-service/Service1.svc.cs
+++ xml-service/xml-service/Service1.svc.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.IO;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Xsl;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
@@ -8,26 +13,74 @@ using System.Text;
 
 namespace xml_service
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
-    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
-    public class Service1 : IService1
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "XmlService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select XmlService.svc or XmlService.svc.cs at the Solution Explorer and start debugging.
+    public class XmlService : IXmlService
     {
-        public string GetData(int value)
+        //Trying 5.1 Web Operation "Verification"
+        //we want to Verify our XML
+
+        public string validateXML(string xmlURL, string xsdURL)
         {
-            return string.Format("You entered: {0}", value);
+            //we are going to get the xml file from the url and make an XML Document
+            try
+            {
+                WebRequest xmlRequest = WebRequest.Create(xmlURL);
+                Stream xmlStream = xmlRequest.GetResponse().GetResponseStream();
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.Load(xmlStream);
+                
+            }
+            catch(Exception ex) //for debugging we want to catch exceptions
+            {
+                return ex.Message;
+            }
+
+            //now we need to get our schema to verify our XML vs our XSD 
+            XmlSchemaSet schema = new XmlSchemaSet();
+            schema.Add(null, xsdURL);
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.ValidationType = ValidationType.Schema;
+            settings.Schemas = schema;
+            settings.ValidationEventHandler += new ValidationEventHandler(ValidationCallBack);
+            try
+            {
+                XmlReader reader = XmlReader.Create(xmlURL, settings);
+                while (reader.Read()) ;
+                return "XML is Valid!!";
+            }
+            catch(Exception ex)
+            {
+                return ex.Message;
+            }
         }
 
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
+
+        //Try to do 5.2 Wep Operation "Transform"
+        public string generateHtml(string xmlURL, string xslURL)
         {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
+            // we are going to get the xml file and make an XML Document
+            WebRequest xmlRequest = WebRequest.Create(xmlURL);
+            Stream xmlStream = xmlRequest.GetResponse().GetResponseStream();
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(xmlStream);
+
+            //get the .xslt file from the url and load it to transform the document
+            XslCompiledTransform transform = new XslCompiledTransform();
+            transform.Load(xslURL);
+
+            //now make a Streamwrite to write the finished .hmtl file to disk
+            StreamWriter output = new StreamWriter(System.Web.HttpContext.Current.Server.MapPath("App_Data/transformed.html"));
+            transform.Transform(xmlDoc, null, output);
+            output.Close();
+            //put the file back
+            return File.ReadAllText(System.Web.HttpContext.Current.Server.MapPath("App_Data/transformed.html"));
+        }
+
+        //we need to make a call back function to interrupt and show errors
+        private static void ValidationCallBack(object sender, ValidationEventArgs events)
+        {
+            throw new Exception("WARNING: VALIDATION FAILED\n" + events.Exception.Message);
         }
     }
 }
